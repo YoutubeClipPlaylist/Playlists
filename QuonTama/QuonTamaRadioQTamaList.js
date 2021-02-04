d54fbae2c97f55c79a10088e30cda5b33e2fe838
陳鈞 <jim60105@gmail.com>
2021-02-05 02:11:46 +0800

    修正RadioQTama #0 字幕網址錯誤

--- QuonTama/QuonTamaRadioQTamaList.js
+++ QuonTama/QuonTamaRadioQTamaList.js
@@ -20,7 +20,7 @@ var myPlaylist = (typeof myPlaylist === 'undefined') ? [] : myPlaylist;
      * @type {*[]}
      */
     var newPlaylist = [
-        ['tj-lHz1dJs4', 0, 0, '#0 アニメ『ソードアート・オンライン』を語る！', 'https://gitlab.com/api/v4/projects/23810267/repository/blobs/202b0a979c8414f5c01e640f957d292e32ea1eeb/raw'],
+        ['tj-lHz1dJs4', 0, 0, '#0 アニメ『ソードアート・オンライン』を語る！', 'https://gitlab.com/api/v4/projects/23810267/repository/blobs/bf3af5411306930d280ea3046608b741aa05efa4/raw'],
         ['zbp1ZpZHxNM', 0, 0, '#1 アニメ『はたらく細胞』を語る！', 'https://gitlab.com/api/v4/projects/23810267/repository/blobs/202b0a979c8414f5c01e640f957d292e32ea1eeb/raw'],
         ['Qvfvsgz8MEU', 0, 0, '#2 アニメ『ホリミヤ』を語る！', 'https://gitlab.com/api/v4/projects/23810267/repository/blobs/683bde2e7e19f3a672623e364e73c94c434a403b/raw'],
         ['9LuK87QodS4', 0, 0, '#3 アニメ『進撃の巨人』を語る！', 'https://gitlab.com/api/v4/projects/23810267/repository/blobs/2b49206db57ac2c51d810538b8e3671fd8a6a154/raw'],
