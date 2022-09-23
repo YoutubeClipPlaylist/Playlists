d43111545efaceee73257968056375cf5f6e02c0
陳鈞 <jim60105@gmail.com>
2022-09-23 18:54:06 +0800

    Init AutoGenerator

---
+++ AutoGenerator/AutoGenerator/Models/Channel.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoGenerator.Models;
+
+public interface IChannel
+{
+    public string? ChannelId { get; set; }
+    public string? Singer { get; set; }
+    public string? Singer_eng { get; set; }
+    public string[] ExcludeKeywords { get; set; }
+}
+
+public class Channel: IChannel
+{
+    public string? ChannelId { get; set; }
+    public string? Singer { get; set; }
+    public string? Singer_eng { get; set; }
+    public string[] ExcludeKeywords { get; set; } = Array.Empty<string>();
+}
